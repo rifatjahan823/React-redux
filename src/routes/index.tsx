9de8a1d7205c80dfe0a4dee8d5852b34ddce407b
rifatjahan823 <rifatjahan823@gmail.com>
2023-09-25 22:28:55 +0600

    add git router

---
+++ src/routes/index.tsx
@@ -0,0 +1,25 @@
+import {
+    createBrowserRouter,
+  } from "react-router-dom";
+import App from "../App";
+import Login from "../page/Login";
+import NotFound from "../page/NotFound";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+    {
+      path: "*",
+      element: <NotFound/>,
+    },
+  ]);
+
+
+  export default router
