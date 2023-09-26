a900c14d50282eb44865990a8ef7b28bddf96a5a
rifatjahan823 <rifatjahan823@gmail.com>
2023-09-26 08:34:17 +0600

    create increment decrement button by redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { decrement, increment, incrementByAmount } from './redux/featurs/counter/counterSlice';
+import { useAppDispatch, useAppSelector } from './redux/hooks';
+
 function App() {
+  const count = useAppSelector((state) => state.Counter.value)
+  const dispatch = useAppDispatch()
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">The Tech net</h1>
+      <div className="flex m-8">
+          <button onClick={()=>dispatch(increment())} className="border-double border-4 border-sky-500 px-3 py-1">Increment</button>
+          <div className="mx-4">{count}</div>
+          <button  onClick={()=>dispatch(decrement())} className="border-double border-4 border-sky-500 px-3 py-1 mx-4">Decrement</button>
+
+          <button  onClick={()=>dispatch(incrementByAmount(5))} className="border-double border-4 border-sky-500 px-3 py-1">Increment by value</button>
+      </div>
     </>
   );
 }
---
+++ src/redux/featurs/counter/counterSlice.tsx
@@ -0,0 +1,29 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+
+export type CounterState={
+    value:number
+}
+
+const initialState:CounterState={
+    value:5
+}
+
+export const counterSlice=createSlice({
+    name:'counter',
+    initialState,
+    reducers:{
+        increment:(state)=>{
+            state.value=state.value+1
+        },
+        decrement:(state)=>{
+            state.value=state.value-1
+        },
+        incrementByAmount:(state,action:PayloadAction<number>)=>{
+            state.value+=action.payload
+        }
+    }
+})
+
+export const{ increment, decrement, incrementByAmount }=counterSlice.actions
+
+export default counterSlice.reducer
