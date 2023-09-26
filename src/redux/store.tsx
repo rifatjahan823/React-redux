d4bf2057a484e143fc7b546e0f585983a5a3411a
rifatjahan823 <rifatjahan823@gmail.com>
2023-09-26 08:48:10 +0600

    add redux middleware

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterSlice from './featurs/counter/counterSlice'
+import counterSlice from './featurs/counter/counterSlice';
+import logger from 'redux-logger'
 
  const store = configureStore({
   reducer: {
     Counter:counterSlice
   },
+  middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(logger)
 })
 
 
